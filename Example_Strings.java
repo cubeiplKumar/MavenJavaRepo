42b9056a81acb9a5b6275bf73ac6a3c737878724
cubeiplkumar <trainings.anil@gmail.com>
2018-11-06 15:16:33 +0530

    New files added for the maven job

---
+++ Example_Strings.java
@@ -0,0 +1,17 @@
+public class Example_Strings{  
+   public static void main(String args[]){  
+	//creating a string by java string literal 
+	String str = "Beginnersbook"; 
+	char arrch[]={'M','a','v','e','n','J','a','v','a'}; 
+	//converting char array arrch[] to string str2
+	String str2 = new String(arrch); 
+		
+	//creating another java string str3 by using new keyword 
+	String str3 = new String("Java String Example"); 
+		
+	//Displaying all the three strings
+	System.out.println(str);  
+	System.out.println(str2);  
+	System.out.println(str3);  
+   }
+}
